912c38c5f399ead345288c3540857d4d9aad6d0d
syuzyuzyu <syuzyuzyu@gmail.com>
2014-11-22 13:18:42 +0900

    my answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
+  BOURNUS_CONDITION = 3
   def self.total_count_for(amount)
-    0
+	  total = add_bornus(amount)  	
+    return total
+  end
+
+  private
+  def self.add_bornus(amount)
+  	bornus_count = 0
+    BOURNUS_CONDITION.upto(amount) do |piece| 
+      bornus_count += 
+      	((piece + bornus_count) % BOURNUS_CONDITION == 0)? 1 : 0
+    end
+    return amount + bornus_count
   end
 end
